e6e939cfe0c00c29ad8db7bd192876b9ae4c2c2c
White House <White House@DESKTOP-QRB46S1>
2021-06-03 00:38:41 +0530

    Add project files.

---
+++ WindowsForms/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace WindowsForms
+{
+    partial class WinForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.labelHobbies = new System.Windows.Forms.Label();
+            this.labelSearch = new System.Windows.Forms.Label();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.textHobbies = new System.Windows.Forms.TextBox();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.textNumber = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.labelCountText = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(165, 30);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(100, 20);
+            this.textName.TabIndex = 0;
+            this.textName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textName_KeyPress);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(62, 30);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name";
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(223, 69);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioFemale.TabIndex = 2;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(49, 300);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(533, 138);
+            this.dataGridView.TabIndex = 3;
+            this.dataGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseClick);
+            // 
+            // labelHobbies
+            // 
+            this.labelHobbies.AutoSize = true;
+            this.labelHobbies.Location = new System.Drawing.Point(62, 169);
+            this.labelHobbies.Name = "labelHobbies";
+            this.labelHobbies.Size = new System.Drawing.Size(46, 13);
+            this.labelHobbies.TabIndex = 5;
+            this.labelHobbies.Text = "Hobbies";
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.AutoSize = true;
+            this.labelSearch.Location = new System.Drawing.Point(62, 259);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(113, 13);
+            this.labelSearch.TabIndex = 6;
+            this.labelSearch.Text = "Search By Name or ID";
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.Location = new System.Drawing.Point(62, 103);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(63, 13);
+            this.labelNumber.TabIndex = 7;
+            this.labelNumber.Text = "Ph. Number";
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(169, 69);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(48, 17);
+            this.radioMale.TabIndex = 8;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // textHobbies
+            // 
+            this.textHobbies.Location = new System.Drawing.Point(165, 144);
+            this.textHobbies.Multiline = true;
+            this.textHobbies.Name = "textHobbies";
+            this.textHobbies.Size = new System.Drawing.Size(289, 63);
+            this.textHobbies.TabIndex = 9;
+            this.textHobbies.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textHobbies_KeyPress);
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(182, 259);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(182, 20);
+            this.textSearch.TabIndex = 10;
+            this.textSearch.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textNumber
+            // 
+            this.textNumber.Location = new System.Drawing.Point(165, 103);
+            this.textNumber.MaxLength = 10;
+            this.textNumber.Name = "textNumber";
+            this.textNumber.Size = new System.Drawing.Size(100, 20);
+            this.textNumber.TabIndex = 11;
+            this.textNumber.TextChanged += new System.EventHandler(this.textNumber_TextChanged);
+            this.textNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textNumber_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(593, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 50);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Delete";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Lime;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(593, 121);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(124, 41);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(262, 221);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(120, 13);
+            this.labelCount.TabIndex = 14;
+            this.labelCount.Text = "Max allowed character: ";
+            // 
+            // labelCountText
+            // 
+            this.labelCountText.AutoSize = true;
+            this.labelCountText.Location = new System.Drawing.Point(388, 221);
+            this.labelCountText.Name = "labelCountText";
+            this.labelCountText.Size = new System.Drawing.Size(10, 13);
+            this.labelCountText.TabIndex = 15;
+            this.labelCountText.Text = "-";
+            this.labelCountText.Click += new System.EventHandler(this.labelCountText_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Lime;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(593, 56);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(124, 41);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // WinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.labelCountText);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textNumber);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.textHobbies);
+            this.Controls.Add(this.radioMale);
+            this.Controls.Add(this.labelNumber);
+            this.Controls.Add(this.labelSearch);
+            this.Controls.Add(this.labelHobbies);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.radioFemale);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textName);
+            this.Name = "WinForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label labelHobbies;
+        private System.Windows.Forms.Label labelSearch;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.TextBox textHobbies;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.TextBox textNumber;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label labelCountText;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ WindowsForms/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsForms
+{
+    public partial class WinForm : Form
+    {
+        // Server name - DESKTOP-QRB46S1\SQLEXPRESS
+        string path = "Data Source=DESKTOP-QRB46S1\\SQLEXPRESS;initial catalog=person;integrated security=true";
+        SqlConnection sqlConnection;
+        SqlCommand cmd;
+        DataTable dataTable;
+        SqlDataAdapter adpt;
+        //Unique ID i.e user_id
+        int ID;
+
+
+        public WinForm()
+        {
+            InitializeComponent();
+            textName.MaxLength = 30;
+            sqlConnection = new SqlConnection(path);
+            displayGridView();
+
+            // Button can only be enabled only if you click on the grid view data
+            btnUpdate.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (textName.Text == "")
+            {
+                MessageBox.Show("Please enter your name", "Name Validation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textNumber.Text == "")
+            {
+                MessageBox.Show("Please enter the mobile number", "Mobile Validation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (!radioMale.Checked && !radioFemale.Checked)
+            {
+                MessageBox.Show("Please select Gender", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textHobbies.Text == "")
+            {
+                MessageBox.Show("Please enter your hobbies", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textNumber.Text.Length < 10)
+            {
+
+                MessageBox.Show("Please enter 10 digit mobile number", "Mobile number Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                try
+                {
+                    string gender;
+                    if (radioMale.Checked)
+                    {
+                        gender = "Male";
+                    }
+                    else
+                    {
+                        gender = "Female";
+                    }
+                    sqlConnection.Open();
+                    cmd = new SqlCommand("insert into users (user_name,gender,ph_number,hobbies) values ('" + textName.Text + "','" + gender + "','" + textNumber.Text + "','" + textHobbies.Text + "')", sqlConnection);
+                    cmd.ExecuteNonQuery();
+                    sqlConnection.Close();
+                    MessageBox.Show("Your Data has been saved");
+                    clearData();
+                    displayGridView();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+            }
+        }
+        private void textName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (!char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                    DialogResult daName = MessageBox.Show("Pleae enter valid name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void textNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (!char.IsDigit(e.KeyChar)&& e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                    DialogResult daNumber = MessageBox.Show("Please enter valid mobile number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void textHobbies_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (!char.IsLetterOrDigit(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar)&& e.KeyChar != (char)Keys.Back && !char.IsSeparator(e.KeyChar) && !char.IsPunctuation(e.KeyChar))
+                {
+                    e.Handled = true;
+                    DialogResult daHobbies = MessageBox.Show("Enter valid details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                labelCountText.Text = (textHobbies.Text.Length+1).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void textNumber_TextChanged(object sender, EventArgs e)
+        {
+            if (textNumber.Text.Length == 10)
+            {
+                textNumber.ForeColor = Color.Black;
+            }
+            else {
+                textNumber.ForeColor = Color.Red;
+            }
+        }
+
+        public void displayGridView() {
+            try
+            {
+                dataTable = new DataTable();
+                sqlConnection.Open();
+                adpt = new SqlDataAdapter("select * from users", sqlConnection);
+                adpt.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+        public void clearData() {
+            textName.Text = "";
+            textNumber.Text = "";
+            textHobbies.Text = "";
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string gender;
+                if (radioMale.Checked)
+                {
+                    gender = "Male";
+                }
+                else
+                {
+                    gender = "Female";
+                }
+                sqlConnection.Open();
+                cmd = new SqlCommand("update users set user_name='" + textName.Text + "',gender='" + gender + "',ph_number='" + textNumber.Text + "',hobbies='" + textHobbies.Text + "' where user_id='"+ID+"'", sqlConnection);
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                MessageBox.Show("Your Data has been updated");
+                
+                displayGridView();
+                clearData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void labelCountText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ID = int.Parse(dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString());
+            textName.Text= dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            radioMale.Checked = true;
+            radioFemale.Checked = false;
+
+            if (dataGridView.Rows[e.RowIndex].Cells[2].Value.ToString()=="Female") {
+                radioMale.Checked = false;
+                radioFemale.Checked = true;
+
+            }
+
+            textNumber.Text= dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+            textHobbies.Text = dataGridView.Rows[e.RowIndex].Cells[4].Value.ToString();
+
+            // Button can only be enabled only if you click on the grid view data
+            btnUpdate.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            adpt=new SqlDataAdapter("select * from users  where user_name like '%"+ textSearch.Text + "%' or user_id like '%" + textSearch.Text + "%'",sqlConnection);
+            dataTable = new DataTable();
+            adpt.Fill(dataTable);
+            dataGridView.DataSource = dataTable;
+            sqlConnection.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                sqlConnection.Open();
+                cmd = new SqlCommand("delete from users where user_id='" + ID + "'", sqlConnection);
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                MessageBox.Show("Your Data has been Deleted");
+
+                displayGridView();
+                clearData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+
+    // Thank You 
+}
